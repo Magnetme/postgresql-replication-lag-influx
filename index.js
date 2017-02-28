973eebad30dbdc00123a43484c2d790366984910
Rogier Slag <rogier.slag@gmail.com>
2017-03-01 00:16:04 +0100

    Fix it finally

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ CASE
 WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location() THEN 0
 ELSE EXTRACT (EPOCH FROM now() - pg_last_xact_replay_timestamp())
 END
-AS replication_lag`;
+AS lag`;
 
 console.log(`Cluster name is ${CLUSTER_NAME}`);
 const pokeAll = () => process.env.SERVERS.split(',').map(url => {
