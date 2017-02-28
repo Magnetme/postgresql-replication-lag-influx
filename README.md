bba8e2797902504cafcd202653227cb0ac440b2c
Rogier Slag <rogier.slag@gmail.com>
2017-02-28 15:36:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+[![Magnet.me Logo](https://cdn.magnet.me/images/logo-2015-full.svg)](https://magnet.me?ref=github-pg-lag "Discover the best companies, jobs and internships at Magnet.me")
+
+[![GitHub release](https://img.shields.io/github/release/magnetme/postgresql-replication-lag-influx.svg)](https://github.com/Magnetme/postgresql-replication-lag-influx/releases)
+[![Docker pulls](https://img.shields.io/docker/pulls/magnetme/postgresql-replication-lag-influx.svg)](https://hub.docker.com/r/magnetme/postgresql-replication-lag-influx/)
+[![Docker build](https://img.shields.io/docker/automated/magnetme/postgresql-replication-lag-influx.svg)](https://hub.docker.com/r/magnetme/postgresql-replication-lag-influx/)
+[![Code Climate](https://img.shields.io/codeclimate/github/magnetme/postgresql-replication-lag-influx.svg)](https://codeclimate.com/github/Magnetme/postgresql-replication-lag-influx)
+[![Github stars](https://img.shields.io/github/stars/magnetme/postgresql-replication-lag-influx.svg?style=social&label=Star)](https://github.com/Magnetme/postgresql-replication-lag-influx)
+
+# postgresql-replication-lag-influx
+
+We use the following simple script/Docker container at [Magnet.me](https://magnet.me?ref=github-pg-lag "Discover the best companies, jobs and internships at Magnet.me") monitor the lag or our postgresql slaves continuously.
+
+You can use the container like this:
+ 
+ ```bash
+ docker run \
+	 -e INFLUX_URL=http://root:root@localhost:8096/infra \
+	 -e RATE=250 \
+	 -e PGUSER=replication \ 
+	 -e PGPASSWORD=yoursecret \ 
+	 -e SERVERS=localhost:5433,localhost:5434 \
+	 magnetme/postgresql-replication-lag-influx
+ ```
---
+++ index.js
@@ -0,0 +1,62 @@
+const pg = require('pg');
+const Influx = require('influx');
+
+const IS_DEBUG = (process.env.DEBUG && process.env.DEBUG.toLowerCase() === 'true') || false;
+const RATE = (process.env.RATE && parseInt(process.env.RATE, 10)) || 2500;
+const CLUSTER_NAME = process.env.CLUSTER_NAME || 'unknown';
+
+const pokeAll = () => process.env.SERVERS.split(',').map(url => {
+	const splitted = url.split(':');
+	return {
+		host : splitted[0],
+		port : splitted[1] || 5432,
+		database : 'postgres'
+	};
+}).forEach(config => poke(config));
+
+const poke = (config) => {
+	const client = new pg.Client(config);
+	const url = `${config.host}:${config.port}`;
+
+	client.connect(err => {
+		if (err) throw err;
+
+		// execute a query on our database
+		client.query('SELECT pg_is_in_recovery() as is_slave;', [], (err, result) => {
+			if (err) throw err;
+
+			// just print the result to the console
+			if (result.rows[0].is_slave) {
+				// Server is a slave, fetch the replication lag
+				client.query('SELECT EXTRACT(EPOCH FROM (now() - pg_last_xact_replay_timestamp())) as lag;', [], (err, result) => {
+					if (err) throw err;
+
+					const lagInMs = Math.round(result.rows[0].lag * 1000);
+					if(IS_DEBUG) console.log(`${url} had a replication of ${lagInMs} ms`);
+
+					setTimeout(() => poke(config), RATE);
+					influx.writeMeasurement(`db.postgres.${CLUSTER_NAME}.lag`, [
+						{
+							tags: { host: config.host, port: config.port },
+							fields: { lag: lagInMs },
+						}
+					])
+				});
+			} else {
+				if (IS_DEBUG) {
+					console.warn(`${url} is not a slave server`);
+				}
+				setTimeout(() => poke(config), RATE);
+			}
+
+			// disconnect the client
+			client.end((err) => {
+				if (err) throw err;
+			});
+		});
+	});
+};
+
+const influx = new Influx.InfluxDB(process.env.INFLUX_URL);
+
+pokeAll();
