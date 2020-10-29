ba712eac6b57253b4a31922e342d683230e3e5b3
Michael de Jong <michaelj@minicom.nl>
2020-10-29 17:10:07 +0100

    Support PG version >= 10

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const CLUSTER_NAME = process.env.CLUSTER_NAME || null;
 
 const LAG_QUERY = `SELECT
 CASE
-WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location() THEN 0
+WHEN pg_last_wal_receive_lsn() = pg_last_wal_replay_lsn() THEN 0
 ELSE EXTRACT (EPOCH FROM now() - pg_last_xact_replay_timestamp())
 END
 AS lag`;
